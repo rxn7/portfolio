6cfb17aceb3117dcb5ee8e051540d2a3b1617760
rxn7 <rotthin_dev@protonmail.com>
2023-01-23 13:26:14 +0100

    Minor optimizations

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -18,16 +18,22 @@ function HeaderNavLink(props: IHeaderNavLinkProps) {
 	)
 }
 
+function NavBar() {
+	return (
+		<nav>
+			<HeaderNavLink path="/" text="Home" />
+			<HeaderNavLink path="/projects" text="Projects" />
+			<HeaderNavLink path="/skills" text="Skills" />
+			<HeaderNavLink path="/experience" text="Experience" />
+			<HeaderNavLink path="/contact" text="Contact" />
+		</nav>
+	)
+}
+
 export default function Header() {
 	return (
 		<header>
-			<nav>
-				<HeaderNavLink path="/" text="Home" />
-				<HeaderNavLink path="/projects" text="Projects" />
-				<HeaderNavLink path="/skills" text="Skills" />
-				<HeaderNavLink path="/experience" text="Experience" />
-				<HeaderNavLink path="/contact" text="Contact" />
-			</nav>
+			<NavBar />
 		</header>
 	)
 }
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -3,15 +3,15 @@ import {Link} from 'react-router-dom'
 import Frame from '../frame/Frame'
 import projects from '../../data/projects.json'
 
-export default function Projects() {
-	const projectsElements: JSX.Element[] = projects.map(project => (
-		<div className="project-container" key={project.name}>
-			<h3 className="project-name title">{project.displayName}</h3>
-			<p className="project-description">{project.description}</p>
-			<Link to={`/project/${project.name}`}>Learn more</Link>
-		</div>
-	))
+const projectsElements: JSX.Element[] = projects.map(project => (
+	<div className="project-container" key={project.name}>
+		<h3 className="project-name title">{project.displayName}</h3>
+		<p className="project-description">{project.description}</p>
+		<Link to={`/project/${project.name}`}>Learn more</Link>
+	</div>
+))
 
+export default function Projects() {
 	return (
 		<Frame maxWidth="1000px" title="My Projects">
 			<div id="projects-container">{projectsElements}</div>
