52bea458b00405200487383d9149302e4e0b3bee
rxn7 <rotthin_dev@protonmail.com>
2023-07-30 22:16:39 +0200

    Fixed header overflow

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,7 @@ import './Header.css'
 
 export default function Header() {
 	return (
-		<header style={{width: "100vw", backgroundColor: 'var(--primary-background-color)', borderBottom: '2px solid var(--secondary-background-color)', display: 'flex', position: 'sticky', top: 0, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', zIndex: 1000, paddingTop: 20, paddingBottom: 20, marginBottom: 20}}>
+		<header style={{width: "100%", backgroundColor: 'var(--primary-background-color)', borderBottom: '2px solid var(--secondary-background-color)', display: 'flex', position: 'sticky', top: 0, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', zIndex: 1000, paddingTop: 20, paddingBottom: 20, marginBottom: 20}}>
 		<Link to='/'><p style={{color: 'var(--primary-color)', margin: 0, fontSize: '2em', userSelect: 'none', cursor: 'pointer'}}>Maciej Niziołek</p></Link>
 			<NavBar />
 		</header>
